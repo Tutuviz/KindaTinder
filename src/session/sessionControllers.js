9a4f0266098f5b9e7ae0483d4dfee0a19dce8e10
Arthur Passos <arthur.opassos@gmail.com>
2020-05-06 18:03:29 -0300

    chore: Organization

--- src/session/sessionControllers.js
+++ src/session/sessionControllers.js
@@ -1,38 +1,36 @@
-const bcrypt = require ("bcryptjs");
-const jwt = require ("jsonwebtoken");
-const Session = require ("./sessionModel")
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../user/userModel');
 
-const verifyPassword = async (password, hash) => {
-    return bcrypt.compare(password, hash);
-}
+const verifyPassword = async (password, hash) => bcrypt.compare(password, hash);
 
 const auth = async (req, res) => {
-    const { email, password } = req.body;
+	const { email, password } = req.body;
 
-    response = await Session.verify(email);
+	const response = await User.verify(email);
 
-    if (!response[0].id || response.error ) {
-        return res.json ({
-            error: 503,
-            message: "Internal Error"
-        });
-    };
+	if (!response.id || response.error) {
+		return res.json({
+			error: 503,
+			message: 'Internal Error',
+		});
+	}
 
-    const passwordMatch = await verifyPassword(password, response[0].password_hash);
+	const passwordMatch = await verifyPassword(
+		password,
+		response.password_hash,
+	);
 
-    if (passwordMatch) {  
-        jwt.sign(response[0].id, process.env.SECRET, (err, token) => {
-            return res.json({
-                token,
-            });
-        });
-    } else {
-        return res.json ({
-            error: 401,
-            message: "Unauthorized"
-        });
-    }
-        
-}
+	if (passwordMatch) {
+		jwt.sign(response.id, process.env.SECRET, (err, token) => res.json({
+			token,
+		}));
+	} else {
+		return res.json({
+			error: 401,
+			message: 'Unauthorized',
+		});
+	}
+};
 
 module.exports = { auth };
--- src/user/userModel.js
+++ src/user/userModel.js
@@ -1,149 +1,194 @@
-const Pool = require ("../utils/db/db");
+const Pool = require('../utils/db/db');
+
+const DEFAULT_ERR = {
+	error: 503,
+	message: 'Internal Error',
+};
+
+const verify = async (email) => {
+	try {
+		const {
+			rows,
+		} = await Pool.query(
+			'SELECT id, password_hash FROM users WHERE email = $1',
+			[email],
+		);
+		return rows.shift();
+	} catch (err) {
+		return DEFAULT_ERR;
+	}
+};
 
 const getMyself = async (id) => {
-    try {
-        const { rows } = await Pool.query(
-            'SELECT * FROM users WHERE id = $1', [id]
-        );
+	try {
+		const { rows } = await Pool.query('SELECT * FROM users WHERE id = $1', [
+			id,
+		]);
 
-        const user = rows[0];
-        if (user.deleted_at) {
-            return {
-                error: 401,
-                message: "Unauthorized",
-            }
-        }
+		const user = rows[0];
+		if (user.deleted_at) {
+			return {
+				error: 401,
+				message: 'Unauthorized',
+			};
+		}
 
-        return rows;
-        
-    } catch (err) {
-        return {
-            error: 503,
-            message: "Internal Error",
-        }
-    }
-}
+		return rows.shift();
+	} catch (err) {
+		return DEFAULT_ERR;
+	}
+};
+
 const get = async (id) => {
-    try {
+	try {
+		const { rows } = await Pool.query('SELECT * FROM users WHERE id = $1', [
+			id,
+		]);
 
-        const { rows } = await Pool.query(
-            'SELECT * FROM users WHERE id = $1', [id]
-        );
+		const user = rows[0];
+		if (user.deleted_at) {
+			return {
+				error: 401,
+				message: 'Unauthorized',
+			};
+		}
 
-        const user = rows[0];
-        if (user.deleted_at) {
-            return {
-                error: 401,
-                message: "Unauthorized",
-            }
-        }
-
-        return rows;
-    } catch (err) {
-        return {
-            error: 503,
-            message: "Internal Error",
-        }
-    }    
-}
+		return rows.shift();
+	} catch (err) {
+		return DEFAULT_ERR;
+	}
+};
 
 const store = async (user) => {
-    const { name, username, email, phone, document_id, google_id, facebook_id, password_hash } = user
-    if (!name || !username || !email || !phone) {
-        return {
-            error: 401,
-            message: "Unauthorized",
-        }
-    }
-    try {
-        const { rows } = await Pool.query(
-            'INSERT INTO users (name, username, email, phone, document_id, google_id, facebook_id, password_hash) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *', [name, username, email, phone, document_id, google_id, facebook_id, password_hash]
-        );
-        return rows;
-    } catch (err) {
-        return {
-            error: 503,
-            message: "Internal Error",
-        }
-    }    
-}
+	const {
+		name,
+		username,
+		email,
+		phone,
+		document_id,
+		google_id,
+		facebook_id,
+		password_hash,
+	} = user;
 
-const update = async (user, id)=> {
-    const { name, username, email, phone, document_id, password_hash } = user
-    if (!name || !username || !email || !phone || !document_id || !password_hash) {
-        return {
-            error: 400,
-            message: "Bad Request",
-        }
-    }
+	const userExists = await verify(email);
 
-    const response = await getMyself(id);
-    if (response.error || !response.length) {
-        return {
-            error: 400,
-            message: "Bad Request",
-        }
-    }
-    try {
-        const { rows } = await Pool.query(
-            'UPDATE users SET name = $2, username = $3, email = $4, phone = $5, password_hash = $6, document_id =$7, updated_at = NOW() WHERE id = $1 RETURNING *', [id, name, username, email, phone, password_hash, document_id ]
-        );
-        return rows;
-        } catch (err) {
-            return {
-                error: 503,
-                message: "Internal Error",
-            }
-        }    
-}
+	if (userExists) {
+		return {
+			error: 409,
+			message: 'User already exists',
+		};
+	}
 
-const confirm = async (id)=> {
-    try {
-        const { rows } = await Pool.query(
-            'SELECT ${id} FROM users'
-        );
-        return rows;
-        } catch (err) {
-            return {
-                error: 503,
-                message: "Internal Error",
-            }
-        }    
-}
+	try {
+		const {
+			rows,
+		} = await Pool.query(
+			'INSERT INTO users (name, username, email, phone, document_id, google_id, facebook_id, password_hash) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
+			[
+				name,
+				username,
+				email,
+				phone,
+				document_id,
+				google_id,
+				facebook_id,
+				password_hash,
+			],
+		);
+		return rows.shift();
+	} catch (err) {
+		return DEFAULT_ERR;
+	}
+};
 
-const disable = async (id)=> {
-    const response = await getMyself(id);
-    if (response.error || !response.length) {
-        return {
-            error: 400,
-            message: "Bad Request",
-        }
-    }
-    try {
-        const { rows } = await Pool.query(
-            'UPDATE users SET deleted_at = NOW() WHERE id = $1 RETURNING *', [id]
-        );
-        return rows;
-    } catch (err) {
-        return {
-            error: 503,
-            message: "Internal Error",
-        }
-    }
-}
+const update = async (user, id) => {
+	const {
+		name, username, email, phone, document_id, password_hash,
+	} = user;
+	if (
+		!name
+    || !username
+    || !email
+    || !phone
+    || !document_id
+    || !password_hash
+	) {
+		return {
+			error: 400,
+			message: 'Bad Request',
+		};
+	}
+
+	const response = await getMyself(id);
+	if (response.error || !response.length) {
+		return {
+			error: 400,
+			message: 'Bad Request',
+		};
+	}
+	try {
+		const {
+			rows,
+		} = await Pool.query(
+			'UPDATE users SET name = $2, username = $3, email = $4, phone = $5, password_hash = $6, document_id =$7, updated_at = NOW() WHERE id = $1 RETURNING *',
+			[id, name, username, email, phone, password_hash, document_id],
+		);
+		return rows.shift();
+	} catch (err) {
+		return DEFAULT_ERR;
+	}
+};
+
+const confirm = async () => {
+	try {
+		const { rows } = await Pool.query();
+		return rows.shift();
+	} catch (err) {
+		return DEFAULT_ERR;
+	}
+};
+
+const disable = async (id) => {
+	const response = await getMyself(id);
+	if (response.error || !response.length) {
+		return {
+			error: 400,
+			message: 'Bad Request',
+		};
+	}
+	try {
+		const {
+			rows,
+		} = await Pool.query(
+			'UPDATE users SET deleted_at = NOW() WHERE id = $1 RETURNING *',
+			[id],
+		);
+		return rows.shift();
+	} catch (err) {
+		return DEFAULT_ERR;
+	}
+};
 
 const upload = async (url) => {
-    try {
-        const { rows } = await Pool.query(
-            'INSERT INTO users_pictures url = $1 RETURNING *', [url]
-        )
-        return rows;
-    } catch (err) {
-        return {
-            error: 503,
-            message: "Internal Error",
-        }
-    }
-}
+	try {
+		const {
+			rows,
+		} = await Pool.query('INSERT INTO users_pictures url = $1 RETURNING *', [
+			url,
+		]);
+		return rows;
+	} catch (err) {
+		return DEFAULT_ERR;
+	}
+};
 
-module.exports = { getMyself, get, store, update, confirm, disable, upload }
+module.exports = {
+	getMyself,
+	get,
+	store,
+	update,
+	confirm,
+	disable,
+	upload,
+};
