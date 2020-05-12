4a6fdfd4f531d2c05d07696ad0737b98cdaaab88
Arthur Passos <arthur.opassos@gmail.com>
2020-05-12 16:38:30 -0300

    chore: Save images localy

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ server.use(express.json());
 
 server.use(Routes);
 
+server.use('/static', express.static('uploads'));
+
 server.listen(process.env.PORT, () => {
 	console.log(`Server rodando na porta ${process.env.PORT}`);
 });
--- src/utils/upload/multer.js
+++ src/utils/upload/multer.js
@@ -1,7 +1,5 @@
 const multer = require('multer');
 const path = require('path');
-const multerS3 = require('multer-s3');
-const AWS = require('aws-sdk');
 
 const checkFile = (req, file, next) => {
 	const ext = path.extname(file.originalname);
@@ -13,36 +11,7 @@ const checkFile = (req, file, next) => {
 		// });
 		//
 	}
-	// useTabs = true => Eslint = no-tabs
-
-	// useTabs = false => Eslint = Expecting tabs
-
 	return next(null, true);
-};
-
-const s3 = new AWS.S3({
-	accessKeyId: process.env.AWS_ACCESS_KEY,
-	secretAccessKey: process.env.AWS_SECRET_KEY,
-});
-
-const S3Storage = {
-	storage: multerS3({
-		s3,
-		acl: 'public-read',
-		bucket: process.env.AWS_S3_BUCKETNAME,
-		metadata: (req, file, cb) => {
-			cb(null, { fieldName: file.fieldname });
-		},
-		key: (req, file, cb) => {
-			const uniquePrefix = `${Date.now()}-${Math.round(
-				Math.random() * 1e9,
-			)}`;
-			const fileExtension = path.extname(file.originalname);
-			cb(null, uniquePrefix + fileExtension);
-		},
-	}),
-	limits: { fileSize: 5 * 1024 * 1024 }, // 5mb
-	fileFilter: checkFile,
 };
 
 const storage = multer.diskStorage({
@@ -57,21 +26,11 @@ const storage = multer.diskStorage({
 	},
 });
 
-const DiskStorage = {
+const Uploader = multer({
 	storage,
-	limits: { fileSize: 5 * 1024 * 1024 }, // 5 mb
+	limits: { fileSize: 5 * 1024 * 1024 },
 	fileFilter: checkFile,
-};
-
-const Uploader = multer(
-	process.env.UPLOAD_STORAGE === 'diskstorage' ? DiskStorage : S3Storage,
-).single('file');
-
-// const Uploader = multer({
-// 	storage,
-// 	limits: { fileSize: 5 * 1024 * 1024 },
-// 	fileFilter: checkFile,
-// }).single('file');
+}).single('file');
 
 const uploadFile = (req, res, next) => {
 	Uploader(req, res, (err) => {
