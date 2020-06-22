fd7d9ab48fdaabe5d59bd634fcb013655515f6bd
Arthur Passos <arthur.opassos@gmail.com>
2020-06-22 19:46:38 -0300

    chore: github action

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		node: true,
+	},
+	extends: ['airbnb-base'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+		camelcase: 0,
+		'object-curly-newline': 0,
+		'operator-linebreak': 0,
+	},
+};
